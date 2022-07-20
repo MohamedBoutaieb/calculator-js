03f07d4b56e557d209fd88a4ce91048c09241eb3
MohamedBoutaieb <mohamedboutaieb1459@gmail.com>
2022-07-20 19:28:57 +0100

    first

--- script.js
+++ script.js
@@ -57,13 +57,17 @@ function evaluateExpression(str) {
 
     }
     // if the result is Nan then something was wrong with the input
-    if (parseInt(terms[0])===NaN)
-        return 'Error!';
+    console.log(parseInt(terms[0]))
+    if (parseInt(terms[0])!=0 && !parseInt(terms[0]))
+        {
+            return 'Error!';
+        }
     //choose whether to display the result as int or float according to its value
     if (parseFloat(terms[0]).toFixed(3) == parseInt(terms[0])) {
         return parseInt(terms[0]);
     }
     dot.disabled = true;
+    console.log('xxxxxxxxxx')
     return parseFloat(terms[0]).toFixed(3);
 
 }
